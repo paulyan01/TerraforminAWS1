6e21f589f9f5a1ac1aa750914dfc9a6530f6b2ea
paulyan01 <126923309+paulyan01@users.noreply.github.com>
2023-05-03 19:35:04 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "sa-east-1"
+  region = "us-east-1"
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_SECRET_KEY
 }
 resource "aws_instance" "web" {
-  ami           = "ami-02dc8ad50da58fffd"
-  instance_type = "t3.micro"
+  ami           = "ami-0889a44b331db0194"
+  instance_type = "t2.micro"
   
   tags = {
     name = "HelloWorld"
